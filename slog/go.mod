20489cee82ec378410848c3ce0408611cde07d25
Tiago Katcipis <tiagokatcipis@gmail.com>
2023-05-31 12:41:36 +0200

    feat: add first slog version

---
+++ slog/go.mod
@@ -0,0 +1,8 @@
+module github.com/birdie-ai/golibs/slog
+
+go 1.20
+
+require (
+	github.com/google/uuid v1.3.0
+	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1
+)
---
+++ slog/slog_test.go
@@ -0,0 +1,81 @@
+package slog_test
+
+import (
+	"testing"
+
+	"github.com/birdie-ai/golibs/slog"
+)
+
+func TestLoadConfigDefault(t *testing.T) {
+	config, err := slog.LoadConfig("DEFAULT")
+
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	if config.Level != slog.DefaultLevel {
+		t.Errorf("got %v, want default level %v", config.Level, slog.DefaultLevel)
+	}
+
+	if config.Format != slog.DefaultFormat {
+		t.Errorf("got %v, want default fmt %v", config.Format, slog.DefaultFormat)
+	}
+}
+
+func TestLoadConfig(t *testing.T) {
+	t.Setenv(logLevelEnv, "debug")
+	t.Setenv(logFmtEnv, "text")
+
+	config, err := slog.LoadConfig(service)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	if config.Level != slog.LevelDebug {
+		t.Errorf("got %v, want level %v", config.Level, slog.LevelDebug)
+	}
+
+	if config.Format != slog.FormatText {
+		t.Errorf("got %v, want fmt %v", config.Format, slog.FormatText)
+	}
+}
+
+func TestLoadConfigErr(t *testing.T) {
+	t.Setenv(logLevelEnv, "debug")
+	t.Setenv(logFmtEnv, "wrong")
+
+	config, err := slog.LoadConfig(service)
+	if err == nil {
+		t.Fatalf("expected error, got config: %v", config)
+	}
+
+	t.Setenv(logLevelEnv, "wrong")
+	t.Setenv(logFmtEnv, "text")
+
+	config, err = slog.LoadConfig(service)
+	if err == nil {
+		t.Fatalf("expected error, got config: %v", config)
+	}
+}
+
+func ExampleLoadConfig() {
+	_ = slog.Configure(slog.Config{
+		Level:  slog.LevelDebug,
+		Format: slog.FormatText,
+	})
+
+	slog.Info("info msg", "key", "val", "key2", 666)
+
+	_ = slog.Configure(slog.Config{
+		Level:  slog.LevelDebug,
+		Format: slog.FormatGcloud,
+	})
+
+	slog.Debug("debug msg", "key", "val", "key2", 666)
+}
+
+const (
+	service     = "TEST"
+	logLevelEnv = service + "_LOG_LEVEL"
+	logFmtEnv   = service + "_LOG_FMT"
+)
