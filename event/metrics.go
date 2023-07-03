73c6933854264825f0794ede2755828bff82354e
Tiago Katcipis <tiagokatcipis@gmail.com>
2023-07-03 20:35:02 +0200

    feat: add event.MustRegisterMetrics

--- event/metrics.go
+++ event/metrics.go
@@ -2,6 +2,12 @@ package event
 
 import "github.com/prometheus/client_golang/prometheus"
 
+// MustRegisterMetrics will register all event related metrics on the given registry.
+// If metrics with the same name already exist no the register this function will panic.
+func MustRegisterMetrics(registry *prometheus.Registry) {
+	registry.MustRegister(publishDuration, publishCounter, processCounter, processDuration)
+}
+
 var (
 	publishDuration = prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
@@ -18,6 +24,24 @@ var (
 		prometheus.CounterOpts{
 			Name: "event_publish_total",
 			Help: "Total of published events",
+		},
+		[]string{"status", "name"},
+	)
+	processDuration = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name: "event_process_duration_seconds",
+			Help: "Duration of event processing",
+			Buckets: []float64{
+				.1, .2, .3, .4, .5, .6, .7, .8, .9, 1,
+				2, 3, 4, 5, 10, 15, 20, 30,
+			},
+		},
+		[]string{"status", "name"},
+	)
+	processCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "event_process_total",
+			Help: "Total of processed events",
 		},
 		[]string{"status", "name"},
 	)
