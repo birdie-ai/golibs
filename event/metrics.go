142c1f6c6ca44dd5ea646bd9973859ef675af9a8
Tiago Katcipis <tiagokatcipis@gmail.com>
2023-07-06 16:57:49 +0200

    feat: add metric instrumentation

--- event/metrics.go
+++ event/metrics.go
@@ -1,11 +1,54 @@
 package event
 
-import "github.com/prometheus/client_golang/prometheus"
+import (
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
 
 // MustRegisterMetrics will register all event related metrics on the given registry.
 // If metrics with the same name already exist no the register this function will panic.
 func MustRegisterMetrics(registry *prometheus.Registry) {
 	registry.MustRegister(publishDuration, publishCounter, processCounter, processDuration)
+}
+
+// SampledMessageHandler will instrument the given MessageHandler returning a new one
+// that samples metrics. These will be `event_process_*` metrics using as `name` the
+// given eventName.
+func SampledMessageHandler(handler MessageHandler, eventName string) MessageHandler {
+	return func(msg Message) error {
+		start := time.Now()
+		err := handler(msg)
+		elapsed := time.Since(start)
+		sampleProcess(eventName, elapsed, err)
+		return err
+	}
+}
+
+func samplePublish(name string, elapsed time.Duration, err error) {
+	status := "ok"
+	if err != nil {
+		status = "error"
+	}
+	labels := prometheus.Labels{
+		"status": status,
+		"name":   name,
+	}
+	publishDuration.With(labels).Observe(elapsed.Seconds())
+	publishCounter.With(labels).Inc()
+}
+
+func sampleProcess(name string, elapsed time.Duration, err error) {
+	status := "ok"
+	if err != nil {
+		status = "error"
+	}
+	labels := prometheus.Labels{
+		"status": status,
+		"name":   name,
+	}
+	processDuration.With(labels).Observe(elapsed.Seconds())
+	processCounter.With(labels).Inc()
 }
 
 var (
