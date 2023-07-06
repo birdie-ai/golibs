f76543c5fb7c8ff26b620d6c139983c924ca6207
Guilherme Santos <guilherme.santos@birdie.ai>
2023-07-06 10:18:28 +0100

    Expose ParseLevel and ParseFormat (#12)

--- slog/slog.go
+++ slog/slog.go
@@ -64,12 +64,12 @@ func LoadConfig(service string) (Config, error) {
 	level := os.Getenv(service + "_LOG_LEVEL")
 	format := os.Getenv(service + "_LOG_FMT")
 
-	logFormat, err := validateFormat(format)
+	logFormat, err := ParseFormat(format)
 	if err != nil {
 		return Config{}, err
 	}
 
-	logLevel, err := validateLogLevel(level)
+	logLevel, err := ParseLevel(level)
 	if err != nil {
 		return Config{}, err
 	}
@@ -184,7 +184,8 @@ const (
 	loggerKey key = iota
 )
 
-func validateLogLevel(level string) (Level, error) {
+// ParseLevel parses the string and returns the corresponding [Level].
+func ParseLevel(level string) (Level, error) {
 	level = strings.ToLower(level)
 	switch level {
 	case "info", "":
@@ -202,7 +203,8 @@ func validateLogLevel(level string) (Level, error) {
 	}
 }
 
-func validateFormat(format string) (Format, error) {
+// ParseFormat parses the string and returns the corresponding [Format].
+func ParseFormat(format string) (Format, error) {
 	switch format {
 	case "gcloud", "text":
 		return Format(format), nil
