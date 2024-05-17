ebea589b60d713553334651f69ef6df8cb001b93
Levin Fritz <levin.fritz@gmail.com>
2024-05-17 14:58:04 +0200

    Use start/end instead of from/to

--- xtime/range.go
+++ xtime/range.go
@@ -3,31 +3,31 @@ package xtime
 
 import "time"
 
-// Range defines a time range that contains time t if From <= t < To.
+// Range defines a time range that contains time t if Start <= t < End.
 type Range struct {
-	From, To time.Time
+	Start, End time.Time
 }
 
 // Contains returns true if [t] is within the range.
 func (r Range) Contains(t time.Time) bool {
-	return !t.Before(r.From) && t.Before(r.To)
+	return !t.Before(r.Start) && t.Before(r.End)
 }
 
 // Duration returns the duration of the time range.
 func (r Range) Duration() time.Duration {
-	return r.To.Sub(r.From)
+	return r.End.Sub(r.Start)
 }
 
 // Split returns a list of time ranges of at most [max] length that together make up [r].
 func (r Range) Split(max time.Duration) []Range {
 	var result []Range
-	for max != 0 && r.To.Sub(r.From) > max {
-		next := r.From.Add(max)
+	for max != 0 && r.End.Sub(r.Start) > max {
+		next := r.Start.Add(max)
 		result = append(result, Range{
-			From: r.From,
-			To:   next,
+			Start: r.Start,
+			End:   next,
 		})
-		r.From = next
+		r.Start = next
 	}
 	result = append(result, r)
 	return result
