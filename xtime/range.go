094284a27639bf508f81ba19c6f60b513c9c4280
Tiago Katcipis <tiagokatcipis@gmail.com>
2024-07-01 14:06:44 +0200

    feat: make xtime.Range immutable and ensure invariants

--- xtime/range.go
+++ xtime/range.go
@@ -1,34 +1,58 @@
 // Package xtime extends Go's time. So far it contains an implementation of a time range.
 package xtime
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
-// Range defines a time range that contains time t if Start <= t < End.
+// Range defines a time range ensuring the invariant that [Range.Start()] <= [Range.End()].
 type Range struct {
-	Start, End time.Time
+	start, end time.Time
+}
+
+// Start returns this range's start.
+// It is guaranteed to be <= than [Range.End()].
+func (r Range) Start() time.Time {
+	return r.start
+}
+
+// End returns this range's end.
+// It is guaranteed to be >= than [Range.Start()].
+func (r Range) End() time.Time {
+	return r.end
 }
 
 // Contains returns true if [t] is within the range.
 func (r Range) Contains(t time.Time) bool {
-	return !t.Before(r.Start) && t.Before(r.End)
+	return !t.Before(r.start) && t.Before(r.end)
 }
 
 // Duration returns the duration of the time range.
 func (r Range) Duration() time.Duration {
-	return r.End.Sub(r.Start)
+	return r.end.Sub(r.start)
 }
 
 // Split returns a list of time ranges of at most [max] length that together make up [r].
 func (r Range) Split(max time.Duration) []Range {
 	var result []Range
-	for max != 0 && r.End.Sub(r.Start) > max {
-		next := r.Start.Add(max)
+	for max != 0 && r.end.Sub(r.start) > max {
+		next := r.start.Add(max)
 		result = append(result, Range{
-			Start: r.Start,
-			End:   next,
+			start: r.start,
+			end:   next,
 		})
-		r.Start = next
+		r.start = next
 	}
 	result = append(result, r)
 	return result
+}
+
+// NewRange creates a new [Range] validating start/end.
+// It ensures the invariant that [Range] always has start <= end.
+func NewRange(start, end time.Time) (Range, error) {
+	if start.After(end) {
+		return Range{}, fmt.Errorf("creating range: start %v can't be after end %v", start, end)
+	}
+	return Range{start, end}, nil
 }
