c6afe210e82a8bb6c358a1d2a60ff0dda1c89949
i4k <t.nateldemoura@gmail.com>
2025-11-11 20:13:22 +0100

    feat: introduce bdml.
    Signed-off-by: i4k <t.nateldemoura@gmail.com>

---
+++ bdml/bdml.go
@@ -0,0 +1,44 @@
+package bdml
+
+import "unique"
+
+type (
+	// Stmt is a single bdml statement.
+	Stmt struct {
+		Entity string
+		Op     OpKind
+		Assign Assign
+		Where  Clauses
+	}
+
+	// Stmts is a list of statements.
+	Stmts []Stmt
+
+	// Assign is the field assignments of the operation.
+	// If the key is a dot (".") then it MUST be the only assignment.
+	Assign map[string]any
+
+	// OpKind is the intended operation kind: SET | DELETE
+	OpKind string
+
+	// Clauses is a AND-based list of clause.
+	Clauses []Clause
+
+	// Clause is a filter predicate.
+	Clause struct {
+		Field string
+		Op    LogicalOperator
+		Value any
+	}
+
+	// LogicalOperator is a logical operator. Eg.: "=", "!="
+	LogicalOperator string
+)
+
+var (
+	// As we will process large bulks of statements, this ensures we don't waste memory in redundant information.
+	SET    = unique.Make(OpKind("SET"))
+	DELETE = unique.Make(OpKind("DELETE"))
+	Eq     = unique.Make(LogicalOperator("="))
+	Neq    = unique.Make(LogicalOperator("!="))
+)
