344f504ca3d1378c9b1aa2a29a7bd69dd1be5daf
i4k <t.nateldemoura@gmail.com>
2025-11-17 19:25:01 +0100

    feat: add dml parser.  (#112)
    Signed-off-by: i4k <t.nateldemoura@gmail.com>

--- dml/dml_example_test.go
+++ dml/dml_example_test.go
@@ -1,7 +1,10 @@
 package dml_test
 
 import (
+	"fmt"
+	"maps"
 	"os"
+	"slices"
 	"unique"
 
 	dml "github.com/birdie-ai/golibs/dml"
@@ -24,4 +27,33 @@ func ExampleStmt() {
 	}
 
 	// Output: SET mydata kind.rating=10 WHERE id="test";
+}
+
+func ExampleParse() {
+	stmts, err := dml.Parse([]byte(`
+	  SET feedbacks
+		custom_fields.connector_id = "my connector",
+		custom_fields.name = "test"
+	  WHERE id = "abc";
+
+	  SET feedbacks
+		custom_fields.connector_id = "my other connector",
+		custom_fields.name = "test 2"
+	  WHERE id = "xyz";
+	`))
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(len(stmts))
+	for _, stmt := range stmts {
+		for _, k := range slices.Sorted(maps.Keys(stmt.Assign)) {
+			fmt.Printf("%s: %s\n", k, stmt.Assign[k].(string))
+		}
+	}
+	// Output: 2
+	// custom_fields.connector_id: my connector
+	// custom_fields.name: test
+	// custom_fields.connector_id: my other connector
+	// custom_fields.name: test 2
 }
