fc575d814289f930a759b65b079bfa2d1d024bba
Gustavo Monti <42518882+gustavom2998@users.noreply.github.com>
2026-08-11 14:12:32 -0300

    feat(dql): collect fields (#167)
    - collect fields can be used to collect the field names mentioned within
    the query.
    - It will be used within searchdb to optimize dynamically the custom
    field schema to only include the custom fields that are included within
    the query.

---
+++ dql/collect.go
@@ -0,0 +1,128 @@
+package dql
+
+import (
+	"maps"
+	"slices"
+)
+
+// CollectFields returns all of the field names present within a statement.
+// The output of this function is unordered, and may change with each call.
+func CollectFields(stmt Stmt) []StaticPath {
+	// Use a map to collect fields to avoid needing to dedup the end result
+	fields := map[string]StaticPath{}
+
+	if len(stmt.Fields) > 0 {
+		for _, f := range stmt.Fields {
+			for _, collectedField := range collectFields(f) {
+				fields[collectedField.String()] = collectedField
+			}
+		}
+	}
+
+	if stmt.Where != nil {
+		for _, f := range collectWhereFields(stmt.Where) {
+			fields[f.String()] = f
+		}
+	}
+
+	for _, o := range stmt.OrderBy {
+		fields[o.Field.String()] = o.Field
+	}
+
+	for _, v := range stmt.Aggs {
+		fs := collectAggFields(v)
+		for _, f := range fs {
+			fields[f.String()] = f
+		}
+	}
+
+	if len(fields) > 0 {
+		return slices.Collect(maps.Values(fields))
+	}
+
+	return []StaticPath{}
+}
+
+// collectFields returns the field names for any [Expr].
+func collectFields(e Expr) []StaticPath {
+	fields := []StaticPath{}
+	switch v := e.(type) {
+	case VarExpr:
+		fields = append(fields, []string{v.Value})
+	case ListExpr:
+		for _, i := range v.Items {
+			fields = append(fields, collectFields(i)...)
+		}
+	case ObjectExpr:
+		for _, v := range v.Keyvals {
+			fields = append(fields, collectFields(v)...)
+		}
+	case PathExpr:
+		base := collectFields(v.Base)
+		// PathExpr has no base, returning the collected fields.
+		if len(base) < 1 {
+			return base
+		}
+		// When evaluating the base path, stop going "down" if the current
+		// base is something that isn't a named field.
+		if !isNamedField(v.Base) {
+			return base
+		}
+		path := base[0]
+		for _, s := range v.Steps {
+			if s.Type != FieldStep {
+				break
+			}
+			path = append(path, s.Field)
+		}
+		fields = append(fields, path)
+	case FncallExpr:
+		for _, arg := range v.Args {
+			fields = append(fields, collectFields(arg)...)
+		}
+	}
+
+	return fields
+}
+
+// isNamedField evaluates whether a expression is a named field. Useful for
+// avoiding composite field names which are a mix of base fields and transformed
+// fields, e.g fn(a).c.
+func isNamedField(e Expr) bool {
+	switch e.(type) {
+	case VarExpr, PathExpr:
+		return true
+	}
+
+	return false
+}
+
+func collectWhereFields(q *QueryExpr) []StaticPath {
+	switch q.Type {
+	case predicate:
+		// Predicate type query - where we can actually analyse the individual
+		// values.
+		fields := []StaticPath{q.LHS}
+		fields = append(fields, collectFields(q.RHS)...)
+		fields = append(fields, collectFields(q.Lower.Val)...)
+		fields = append(fields, collectFields(q.Upper.Val)...)
+		return fields
+	default:
+		// Nested queries, collect recuversively
+		fields := []StaticPath{}
+		for _, c := range q.Children {
+			fields = append(fields, collectWhereFields(c)...)
+		}
+		return fields
+	}
+}
+
+func collectAggFields(a Agg) []StaticPath {
+	fields := []StaticPath{}
+
+	fields = append(fields, collectFields(a.Func)...)
+	for _, agg := range a.Children {
+		fields = append(fields, collectAggFields(agg)...)
+	}
+	return fields
+}
---
+++ dql/collect_test.go
@@ -0,0 +1,214 @@
+package dql_test
+
+import (
+	"testing"
+
+	"github.com/birdie-ai/golibs/dql"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+)
+
+func TestCollectFields(t *testing.T) {
+	tests := []struct {
+		name string
+		in   string
+		want []dql.StaticPath
+	}{
+		{
+
+			name: "no fields",
+			in:   "SEARCH feedbacks;",
+			want: []dql.StaticPath{},
+		},
+		{
+
+			name: "selected fields",
+			in:   "SEARCH feedbacks id,text;",
+			want: []dql.StaticPath{
+				{"id"},
+				{"text"},
+			},
+		},
+		{
+
+			name: "selected custom_fields",
+			in:   "SEARCH feedbacks custom_fields.a.b;",
+			want: []dql.StaticPath{
+				{"custom_fields", "a", "b"},
+			},
+		},
+		{
+
+			name: "select path with idx",
+			in:   "SEARCH feedbacks custom_fields.a.[0].b, custom_fields.b.[custom_fields.index].c;",
+			want: []dql.StaticPath{
+				{"custom_fields", "a"},
+				{"custom_fields", "b"},
+			},
+		},
+		{
+
+			name: "order by fields",
+			in:   "SEARCH feedbacks id ORDER BY posted_at DESC;",
+			want: []dql.StaticPath{
+				{"id"},
+				{"posted_at"},
+			},
+		},
+		{
+			name: "function call",
+			in:   `SEARCH feedbacks a, fn(b, c, d), f({"e": e});`,
+			want: []dql.StaticPath{
+				{"a"},
+				{"b"},
+				{"c"},
+				{"d"},
+				{"e"},
+			},
+		},
+		{
+			name: "nested function call",
+			in:   "SEARCH feedbacks a, fn2(fn1(b), c);",
+			want: []dql.StaticPath{
+				{"a"},
+				{"b"},
+				{"c"},
+			},
+		},
+		{
+			name: "function call with select",
+			in:   "SEARCH feedbacks fn(custom_fields).key;",
+			want: []dql.StaticPath{
+				{"custom_fields"},
+			},
+		},
+		{
+			name: "nested function call with select",
+			in:   "SEARCH feedbacks fn(fn2(a).c, b).d, e;",
+			want: []dql.StaticPath{
+				{"a"},
+				{"b"},
+				{"e"},
+			},
+		},
+		{
+			name: "where fields",
+			in: `
+			SEARCH feedbacks
+			WHERE id="1" AND
+				custom_fields.text="x";`,
+			want: []dql.StaticPath{
+				{"id"},
+				{"custom_fields", "text"},
+			},
+		},
+		{
+			name: "where object",
+			in: `
+			SEARCH feedbacks
+			WHERE enriched_fields={"sentiment": custom_fields.expected_sentiment};`,
+			want: []dql.StaticPath{
+				{"enriched_fields"},
+				{"custom_fields", "expected_sentiment"},
+			},
+		},
+		{
+			name: "nested where fields",
+			in: `SEARCH feedbacks id WHERE {
+		    "$and": [
+				{
+					"posted_at": {
+						"$gte": "2026-01-01T00:00:00Z",
+						"$lt": "2027-01-01T00:00:00Z"
+					}
+				}
+			]
+			};`,
+			want: []dql.StaticPath{
+				{"id"},
+				{"posted_at"},
+			},
+		},
+		{
+			name: "where fields rhs",
+			in: `
+			SEARCH feedbacks
+			WHERE custom_fields.a=custom_fields.b AND 
+				c=fn(custom_fields.d).e;`,
+			want: []dql.StaticPath{
+				{"custom_fields", "a"},
+				{"custom_fields", "b"},
+				{"c"},
+				{"custom_fields", "d"},
+			},
+		},
+		{
+			name: "nested where fields rhs",
+			in: `
+			SEARCH feedbacks WHERE {
+		    "$and": [
+				{
+					"posted_at": {
+						"$gte": custom_fields.dt_start,
+						"$lt": custom_fields.dt_end
+					}
+				},
+				{
+					"text": {
+						"$in": [custom_fields.text_allow_list]
+					}
+				},
+				{
+					"custom_fields.category": {
+						"$in": ["category a"]
+					}
+				}
+			]
+			};`,
+			want: []dql.StaticPath{
+				{"posted_at"},
+				{"custom_fields", "dt_start"},
+				{"custom_fields", "dt_end"},
+				{"text"},
+				{"custom_fields", "text_allow_list"},
+				{"custom_fields", "category"},
+			},
+		},
+		{
+			name: "aggs",
+			in: `
+			SEARCH feedbacks AGGS {
+				by_category: terms(category) LIMIT 10,
+				by_month: date_histogram(custom_fields.some_date) {
+					by_labels: terms(labels) LIMIT 3
+				},
+			};
+			`,
+			want: []dql.StaticPath{
+				{"category"},
+				{"custom_fields", "some_date"},
+				{"labels"},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p, err := dql.Parse(tt.in)
+			if err != nil {
+				t.Fatalf("parsing test case input: %v", err)
+			}
+
+			for _, stmt := range p.Stmts {
+				got := dql.CollectFields(stmt)
+				orderOpt := cmpopts.SortSlices(func(a, b dql.StaticPath) bool {
+					return a.String() < b.String()
+				})
+				if diff := cmp.Diff(tt.want, got, orderOpt); diff != "" {
+					t.Fatalf("expected - got +:\n%v", diff)
+				}
+			}
+
+		})
+	}
+}
